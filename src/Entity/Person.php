13e90658f2d438470803e2cff24da27127ba9bb4
Jan242zc <>
2018-11-28 07:43:04 +0000

    du_body_1-5

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,26 @@ class Person
      * @ORM\Column(type="integer")
      */
     private $age;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Person", inversedBy="children")
+     */
+    private $father;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Person", mappedBy="father")
+     */
+    private $children;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Person", inversedBy="children")
+     */
+    private $mother;
+
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +91,61 @@ class Person
     public function setAge(int $age): self
     {
         $this->age = $age;
+
+        return $this;
+    }
+
+    public function getFather(): ?self
+    {
+        return $this->father;
+    }
+
+    public function setFather(?self $father): self
+    {
+        $this->father = $father;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children[] = $child;
+            $child->setFather($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChild(self $child): self
+    {
+        if ($this->children->contains($child)) {
+            $this->children->removeElement($child);
+            // set the owning side to null (unless already changed)
+            if ($child->getFather() === $this) {
+                $child->setFather(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getMother(): ?self
+    {
+        return $this->mother;
+    }
+
+    public function setMother(?self $mother): self
+    {
+        $this->mother = $mother;
 
         return $this;
     }
